7d4136c2cd7402ad3c5ed6ce301005752b8b4944
Marcio Luis da Silva <marcio.luis.da.silva@accenture.com>
2018-01-19 19:58:08 -0200

    mudança no FLOP.java

--- src/main/java/refactorings/FLOP.java
+++ src/main/java/refactorings/FLOP.java
@@ -5,17 +5,8 @@ import java.math.MathContext;
 import java.math.RoundingMode;
 
 /**
- * Frederick Brooks’ “The Mythical Man-Month” p. 102.
  * 
- * Representation is the Essence of Programming
  * 
- * Much more often, strategic breakthrough will come from redoing the
- * representation of the data or tables. This is where the heart of your program
- * lies. Show me your flowcharts and conceal your tables, and I shall be
- * continued to be mystified. Show me your tables, and I won’t usually need your
- * flowcharts; they’ll be obvious.
- *
- * Frederick Brooks’ “The Mythical Man-Month” p. 102.
  */
 public class FLOP {
 
